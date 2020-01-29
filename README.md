7b1a33d6d51638dde086970d7c8eac7441fa189d
XabierGallardo <javiergallard91@gmail.com>
2020-01-29 18:37:36 +0000

    Created Java Quiz and interview question documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Java-Exercices
 Java interview questions, exercices and solutions
+
+You can work with them and try to solve on https://www.compilejava.net/
+
+Or you can try with Eclipse or Netbeans
+
+Have fun!
---
+++ exercices.java
@@ -0,0 +1,77 @@
+//JAVA Quiz///////////////////////////////////////////////////////////////////////
+//1. What is a correct syntax to output "Hello World" in Java?
+System.out.println("Hello World");
+
+//2. Java is short for JavaScript
+//False
+
+//3. How do you insert COMMENTS in Java code?
+//This is a comment
+
+//4. Which data type is used to create a variable that should store text?
+String
+
+//5. How do you create a variable with the numeric value 5?
+int x = 5;
+
+//6. How do you create a variable with the floating number 2.8?
+float x = 2.8f;
+
+//7. Which method can be used to find the length of a string?
+length();
+
+//8. Which operator is used to add together two values?
+//The + sign
+
+//9. The value of a string variable can be surrounded by single quotes
+//False
+
+//10. Which method can be used to return a string in upper case letters?
+toUpperCase();
+
+//11. Which operator can be used to comparte two values?
+==
+
+//12. To declare an array in Java, define the variable type with
+[ ]
+
+//13. Array indexes start with
+0
+
+//14. How do tou create a method in Java?
+methodName()
+
+//15. How do you call a method in Java?
+methodName();
+
+//16. Which keyword is used to create a class in Java?
+class
+
+//17. What is the corerct way to create an object called myObj of MyClass?
+MyClass myObj = new MyClass();
+
+//18. In Java, it is possible to inherit attributes and methods from one class to another?
+//True
+
+//19. Which method can be used to find the highest value of x and y?
+Math.max()
+
+//20. Which operator is used to multiply numbers?
+*
+
+//21. Which keyword is used to import a package from the Java API library?
+import
+
+//22. How do you start writing an if statement in Java?
+if (x > y) {
+	...
+}
+
+//23. How do you start writing a while loop in Java?
+while x > y
+
+//24. Which keyword is used to return a value inside a method?
+return 
+
+//25. Which statement is used to stop a loop?
+break
