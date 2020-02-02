12e6ba4bb84f6b5e9fe1cb3f4332476a1e8c45e0
XabierGallardo <javiergallard91@gmail.com>
2020-02-02 23:59:53 +0000

    Added create student, create subject and print students functions

--- Exercices/Academic_Centre/AcademicCentre.java
+++ Exercices/Academic_Centre/AcademicCentre.java
@@ -9,14 +9,62 @@ public class AcademicCentre {
 	static HashMap<String, Subject> subjectList = new HashMap<String, Subject>();
 	static HashMap<String, Register> registerList = new HashMap<String, Register>();
 	
+	//Student creation method, checking if there's no repetitions
 	public boolean createStudent (String record, String name) {
-		if (studentList.isEmpty() || studentList.containsKey(record)) {
+		if (studentList.isEmpty() || studentList.containsKey(record)) { 
 			Student student = new Student(record, name);
-			studentList.put(record, student);
+			studentList.put(record, student); //record as primary key
 			return true;
 		} else {
 			return false;
 		}
 	}
+	
+	//Subject creation method, checking if there's no repetitions
+	public boolean createSubject(String code, String nameSubject, String credits) {
+		if (subjectList.isEmpty() || subjectList.containsKey(code)) { 
+			Subject subject = new Subject(code, nameSubject, credits);
+			subjectList.put(code, subject);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//Print student method
+	public void printStudent (String record) {
+		if (studentList.isEmpty() || studentList.containsKey(record)) {
+			System.out.println("This student doesn't appear on the list");
+		} else {
+			Student student = AcademicCentre.studentList.get(record);
+			System.out.println(student);
+			
+			//Iterate over register to get subject and qualification
+			for (Register registerStudent : registerList.values()) {
+				if(registerStudent.equals(record)) {
+					System.out.println("Code: " + registerStudent.getCode() + ", qualification: " + registerStudent.getQualification()); 
+				}
+			}
+		}
+	}
+	
+	//Print all students method
+	public void printStudents () {
+		if (studentList.isEmpty()) {
+			System.out.println("Empty list");
+		} else {
+		for (Student student : studentList.values()) {
+			System.out.println(student);
+			
+			for (Register registerStudent : registerList.values()) {
+					
+				//Check if the student is already registered
+				if (registerStudent.getRecord().contentEquals(student.getRecord())) {
+					System.out.println("Code: " + registerStudent.getCode() + ", qualification: " + registerStudent.getQualification());
+					}
+				}
+			}
+	}
 }
+
 
