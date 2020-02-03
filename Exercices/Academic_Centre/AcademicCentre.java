411c3e8e677fa3f140b00582b2c33a83beda8ae0
XabierGallardo <javiergallard91@gmail.com>
2020-02-03 00:31:01 +0000

    Updated instructions, added register, addQualification and studentComparisons methods

--- Exercices/Academic_Centre/AcademicCentre.java
+++ Exercices/Academic_Centre/AcademicCentre.java
@@ -30,6 +30,18 @@ public class AcademicCentre {
 			return false;
 		}
 	}
+
+
+	//Register student method
+	public boolean register (String record, String code) {
+		if(studentList.containsKey(record) && subjectList.containsKey(code)) {
+			Register register = new Register(record, code);
+			registerList.put(record + "-" + code, register);
+			return true;
+		}
+		return false;
+	}
+
 	
 	//Print student method
 	public void printStudent (String record) {
@@ -67,4 +79,54 @@ public class AcademicCentre {
 	}
 }
 
-
+	//Add qualification method
+	public boolean addQualification (String record, String code, double qualification) {
+		Register registerQualification = registerList.get(record + "-" + code);
+		
+		if (registerQualification.getCode().equals(code)) {
+			registerQualification.setQualification(qualification);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//Student comparison method
+	public void studentComparison (String record1, String record2) {
+		
+		Student student1 = studentList.get(record1);
+		Student student2 = studentList.get(record2);
+		Double averageQual1;
+		Double averageQual2;
+		int asignNumStudent1 = 0;
+		Double totalQualStudent1 = null;
+		
+		for (Register registerStudent : registerList.values()) {
+			if (registerStudent.getRecord().contentEquals(student1.getRecord())) {
+				totalQualStudent1 = +registerStudent.getQualification();
+				asignNumStudent1 = +1;
+			}
+		}
+		
+		averageQual1 = totalQualStudent1 / asignNumStudent1;
+		int asignNumStudent2 = 0;
+		Double totalQualStudent2 = null;
+		
+		for (Register registerStudent : registerList.values()) {
+			if (registerStudent.getRecord().contentEquals(student2.getRecord())) {
+				totalQualStudent2 = +registerStudent.getQualification();
+				asignNumStudent2 = +1;
+			}
+		}
+		
+		averageQual2 = totalQualStudent2 / asignNumStudent2;
+		
+		if (averageQual1 > averageQual2) {
+			System.out.println("Student with record: " + record1 + " has superior qualifications than " + record2);
+		}else if (averageQual1 < averageQual2) {
+			System.out.println("Student with record: " + record2 + " has superior qualifications than " + record1);
+	} else {
+		System.out.println("Both students have same qualifications");
+	}
+}
+}
